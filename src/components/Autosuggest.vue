c85dca3a9f6cbeb3863ec1b86269df06fc8baa5c
ideabile <info@ideabile.com>
2018-02-08 07:14:37 +0100

    Latest fixes

--- src/components/Autosuggest.vue
+++ src/components/Autosuggest.vue
@@ -4,7 +4,7 @@
         <div class="suggest">
             <input ref="input" v-on:blur="blur" v-model="value" />
             <ul>
-                <li v-for="suggestion in suggestions" v-on:click="setValue(suggestion)">{{ suggestion }}</li>
+                <li v-for="suggestion in suggestions" v-on:click="setValue(suggestion.value)">{{ suggestion.label }}</li>
             </ul>
         </div>
     </div>
@@ -14,14 +14,51 @@
  .active {
      position: relative;
  }
+
+ input {
+     margin: 4px 2px;
+ }
+
  .suggest {
      display: none;
      position: absolute;
-     top: 100%;
+     top: calc(100% + 27px);
      left: 0;
+     border: 1px solid #d4d4d4;
+     background: linear-gradient(#f4f4f4, #ececec);;
+     z-index: 10;
+     padding: 4px;
+     border-radius: 0 0 4px 4px;
  }
  .active .suggest {
      display: block;
+ }
+
+ ul {
+     max-height: 100px;
+     overflow-y: scroll;
+     list-style: none;
+     padding: 0;
+     margin: 0;
+ }
+
+ ul li {
+     display: block;
+     height: 12px;
+     background: linear-gradient(#f4f4f4, #ececec);;
+     line-height: 12px;
+     padding: 10px;
+     border-top: 1px solid #d4d4d4;
+     border-bottom: 1px solid #d4d4d4;
+ }
+
+ li:last-child {
+     border: 0;
+ }
+
+ li:hover {
+     background: linear-gradient(#ececec, #f4f4f4);
+     cursor: pointer;
  }
 </style>
 
